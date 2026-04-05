3b617ea546dbfcc4bdd517098fc182ca64d302cc
AnishDessai <dessaianish94@gmail.com>
2026-04-05 18:54:00 +0530

    maddy not ho

--- frontend/src/supabaseClient.js
+++ frontend/src/supabaseClient.js
@@ -3,6 +3,4 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = "https://ukissixrretjgwtythxl.supabase.co";
 const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVraXNzaXhycmV0amd3dHl0aHhsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUzMjIwODksImV4cCI6MjA5MDg5ODA4OX0.dkQc_MHk9NIyAvJsnNikHzKS325wnk9Div1vSRp9kzk";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
